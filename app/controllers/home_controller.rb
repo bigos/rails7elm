d9fdea002d4f8c038a870ac4a3ac7bf285d511ff
Jacek Podkanski <ruby.object@googlemail.com>
2023-04-22 00:15:34 +0100

    added collections example

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def page
+    @elm_flags = {user_full_name: "Me Myself",
+                  user_email: "me@example.com",
+                  contact_id: 1234,
+                  branch_id: 5678,
+                  branches: [{"branch_id":1234,
+                              "name": "My Branch 1",
+                              "address": "123 My Street, Someville",
+                              "postcode": "AB C12",
+                              "phone": "01234567"},
+                             {"branch_id":1235,
+                              "name": "My Branch 2",
+                              "address": "456 My Street, Someville",
+                              "postcode": "AB C12",
+                              "phone": "01234568"},
+                             {"branch_id":12346,
+                              "name": "My Branch 3",
+                              "address": "789 My Street, Someville",
+                              "postcode": "AB C12",
+                              "phone": "01234569"}
+                            ],
+                  status: "Ok" }
   end
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.e
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.7/lib/index.js"
 
 pin 'custom-alert'
+pin 'collections'
